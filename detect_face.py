416dfeae89d9521fe1a5e9519a7c4531438234fe
antrixsh <antrixsh@gmail.com>
2018-10-03 18:02:33 +0530

    Add
    Add

---
+++ detect_face.py
@@ -0,0 +1,73 @@
+import cv2
+import numpy as np
+import os 
+from scipy.spatial import distance
+from imutils import face_utils
+from keras.models import load_model
+import dlib
+import glob
+
+detector = dlib.get_frontal_face_detector()
+
+FRmodel = load_model('face-rec_Google.h5')
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+#iniciate id counter
+id = 0
+
+# names related to ids: example ==> Marcelo: id=1,  etc
+names = ['None', 'Rohit', 'Lalit', 'Avrati','Ananth'] 
+
+# Initialize and start realtime video capture
+cam = cv2.VideoCapture(0)
+cam.set(3, 640) # set video widht
+cam.set(4, 480) # set video height
+
+# Define min window size to be recognized as a face
+minW = 0.1*cam.get(3)
+minH = 0.1*cam.get(4)
+
+while True:
+
+    ret, img =cam.read()
+
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale( 
+        gray,
+        scaleFactor = 1.2,
+        minNeighbors = 5,
+        minSize = (int(minW), int(minH)),
+       )
+
+    for(x,y,w,h) in faces:
+
+        cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
+
+        id, confidence = predictor(gray[y:y+h,x:x+w])
+        print('ID:-', id)
+        # Check if confidence is less them 100 ==> "0" is perfect match 
+        if (confidence < 100):
+            id = names[id]
+            
+            confidence = "  {0}%".format(round(100 - confidence))
+        else:
+            id = "unknown"
+            confidence = "  {0}%".format(round(100 - confidence))
+        
+        cv2.putText(img, str(id), (x+5,y-5), font, 1, (255,255,255), 2)
+        cv2.putText(img, str(confidence), (x+5,y+h-5), font, 1, (255,255,0), 1)  
+    
+    cv2.imshow('camera',img) 
+
+    k = cv2.waitKey(10) & 0xff # Press 'ESC' for exiting video
+    if k == 27:
+        break
+
+# Do a bit of cleanup
+print("\n [INFO] Exiting Program and cleanup stuff")
+cam.release()
+cv2.destroyAllWindows()
